bb00a40406ed77d925e35340c9624ab846df8ee8
joaovitormonteiro <joaovitormonteiro@usp.br>
2023-06-17 18:05:47 -0300

    corrige teste

--- src/app/components/views/workspace/team-view/team-view.component.spec.ts
+++ src/app/components/views/workspace/team-view/team-view.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TeamViewComponent } from './team-view.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { WorkspaceSingleComponent } from '../workspace-single/workspace-single.component';
 
 describe('TeamViewComponent', () => {
   let component: TeamViewComponent;
@@ -10,7 +11,8 @@ describe('TeamViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TeamViewComponent ],
+      declarations: [ TeamViewComponent, WorkspaceSingleComponent ],
+      providers: [WorkspaceSingleComponent],
       imports: [
         RouterTestingModule,
         HttpClientTestingModule
