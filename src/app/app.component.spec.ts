370168bdf6a81b753db53da8ad55649f6c1cb8ad
joaovitormonteiro <joaovitormonteiro@usp.br>
2023-06-17 18:05:59 -0300

    remove teste errado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,11 +27,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('sos-tool-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sos-tool-frontend app is running!');
-  });
 });
